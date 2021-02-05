4cb7939ffc941bb0673ed243a1c88f55ba1d9ced
Imdat K <imdatkucuk28@gmail.com>
2021-02-05 23:45:16 +0100

    Guncellemelere devam

--- src/A/yabanci.java
+++ src/A/yabanci.java
@@ -2,6 +2,6 @@ package A;
 
 public class yabanci {
     public static void main(String[] args) {
-        System.out.println("yabanci listesi ");
+        System.out.println("yabanci yabanci duruyor ");
     }
 }
