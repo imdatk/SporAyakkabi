30dfc1b9a5ca8976c07337a0b9282d1e5c47fbde
Imdat K <imdatkucuk28@gmail.com>
2021-02-06 18:15:03 +0100

    abd abc gibi

--- src/A/AyakkabiYerli.java
+++ src/A/AyakkabiYerli.java
@@ -2,7 +2,7 @@ package A;
 
 public class AyakkabiYerli {
     public static void main(String[] args) {
-        System.out.println("HelloWord");
+        System.out.println("Gunaydin Dunya");
     }
 
 }
